f9631c9008a4c49363f8a5cd0b08cc56bfa33d3f
kelvinjou <63611619+kelvinjou@users.noreply.github.com>
2023-06-01 22:49:10 -0700

    Initial Commit

---
+++ CodeFormerGUI/CodeFormerGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeFormerGUIApp.swift
+//  CodeFormerGUI
+//
+//  Created by Kelvin J on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeFormerGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeFormerGUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeFormerGUI
+//
+//  Created by Kelvin J on 6/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
